543441f5d8d8fad3415f783e29fcf63b6eb9c083
belmontchandru <belmont.chandru@gmail.com>
2024-10-07 17:09:10 +0530

    feat: Display Task items using Local Storage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tasks = [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 document.getElementById("taskForm").addEventListener('submit', function(event) {
     event.preventDefault();
@@ -10,12 +10,15 @@ document.getElementById("taskForm").addEventListener('submit', function(event) {
 
     const newTask = {
         name: taskName,
-        description: taskDescription,
+        description: taskDescription || 'Not Mentioned', //if no value
         dueDate: dueDate
     };
 
     // pushing task data in Tasks array
     tasks.push(newTask);
+
+    //local storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
     //empty form inputs
     document.getElementById('taskForm').reset();
@@ -24,35 +27,50 @@ document.getElementById("taskForm").addEventListener('submit', function(event) {
 });
 
 // For Displaying the Tasks in the Task list
-
 function displayTasks() {
-   const taskList = document.getElementById('task-list');
-   taskList.innerHTML = ''; // resetting to null in the begining
+    const displayTaskItem = document.getElementById('display-task-item');
+    const taskContainer = document.querySelector('.task-header');
+    displayTaskItem.innerHTML = ''; // resetting to null in the begining
+
+    if (tasks.length > 0) {
+        taskContainer.style.display = 'grid'; // has value display
+    } else {
+        taskContainer.style.display = 'none'; // Hide if no value
+    }
 
    tasks.forEach((task, index) => {
     const { name, description, dueDate } = task; //destructuring task object
 
-    // Create a table row
-    const row = document.createElement('tr');
+     const taskRow = document.createElement('div');
+     taskRow.className = 'task-container';
 
-    row.innerHTML = `
-            <td>${name}</td>
-            <td>${description}</td>
-            <td>${dueDate}</td>
-            <td><button onclick="removeTask(${index})">Remove</button></td>
-        `;
+     taskRow.innerHTML = `
+        <div>
+            <input type="checkbox" id="checkbox-task3" name="task" class="custom-checkbox">
+        </div>
+        <div>${name}</div>
+        <div>${description} </div>
+        <div>${dueDate}</div>
+        <div>
+            <button class="remove-btn" onclick="removeTask(${index})">🗑️</button>
+        </div>
+    `;
     
-    taskList.appendChild(row);
+    displayTaskItem.appendChild(taskRow);
+
 });
 }
 
 // function for Removing task
 function removeTask(index) {
     tasks.splice(index, 1); // Remove the task at the given index
+
+    //update local storage after task removed
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
     displayTasks(); // Update the displayed task list
 }
 
-// need to destructure and display the array
-
-
-
+window.onload = function() {
+    displayTasks();
+}
